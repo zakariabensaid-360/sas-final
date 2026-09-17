beb416b088653721cf3cdc6a48ff3d774b5b9ed4
zakaria bensaid <zakariabensaid58@gmail.com>
2026-09-17 07:53:29 -0300

    update

---
+++ services/filtrerParNiveau.js
@@ -0,0 +1,38 @@
+import data from "../data.js";
+import { prompt } from "../main.js";
+import calculerProgression from "./calculerProgression.js";
+
+export default function filtrerParNiveau() {
+  console.log(`
+        1) Solide \n
+        2) En progress \n
+        3) a renforce        
+        
+`);
+  let text = prompt("votre choix: ");
+  let niveau;
+
+  if (text == "1") {
+    niveau = "Solide";
+  } else if (text == "2") {
+    niveau = "En progression";
+  } else if (text == "3") {
+    niveau = "À renforcer";
+  }
+
+  let filtred_data = [];
+  for (let i = 0; i < data.length; i++) {
+    let calc = calculerProgression(data[i].id);
+    if (calc.niveau == niveau) {
+      filtred_data.push({
+        id: data[i].id,
+        nomComplet: data[i].nomComplet,
+        niveau: calc.niveau,
+        progress: calc.progression,
+      });
+    }
+  }
+
+  return filtred_data;
+}
+
---
+++ utilities/validerResultat.js
@@ -0,0 +1,9 @@
+
+
+export default function validerResultat(jour, resultats, prompt) {
+    if(isNaN(jour)) {
+        console.log("You must provide the right type of data for jour")
+        return;
+    }
+
+}
