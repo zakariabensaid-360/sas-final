9b9739b152db1a5c2b75d19d8d12be0a8adf7a7c
zakaria bensaid <zakariabensaid58@gmail.com>
2026-09-18 11:21:14 -0300

    update

--- utilities/normaliserNom.js
+++ utilities/normaliserNom.js
@@ -1,7 +1,17 @@
 export default function normaliserNom(str) {
-  const cleanStr = str.replace(/[^a-zA-Z\s]/g, '').trim().toLocaleLowerCase().split(' ')
-  let first = cleanStr[0][0].toLocaleUpperCase() + cleanStr[0].slice(1)
-  let last = cleanStr[1][0].toLocaleUpperCase() + cleanStr[1].slice(1)
+  if (str.split(" ")[1] == undefined || !isNaN(str)) {
+    console.log(
+      "you should add a full name of the user && and should contain string only \n",
+    );
+    return;
+  }
+  const cleanStr = str
+    .replace(/[^a-zA-Z\s]/g, "")
+    .trim()
+    .toLocaleLowerCase()
+    .split(" ");
+  let first = cleanStr[0][0].toLocaleUpperCase() + cleanStr[0].slice(1);
+  let last = cleanStr[1][0].toLocaleUpperCase() + cleanStr[1].slice(1);
 
-  return first + ' ' + last
+  return first + " " + last;
 }
